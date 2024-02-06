5bfd24e5ab342773c8d824c2505df0c04c432b9a
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-02-06 03:55:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var date = new Date()
+document.write(date.getFullYear() + "<br>");
+document.write(date.getDate()+ "<br>" );
+document.write(date.getMinutes()+ "<br>");
+document.write(date.getSeconds()+ "<br>");
+document.write(date.getDay());
